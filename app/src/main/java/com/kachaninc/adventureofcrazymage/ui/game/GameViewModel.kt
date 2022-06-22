1139b6aa943a711d3673000e0a6fda9db372b63d
leo <caladdir@gmail.com>
2022-06-22 11:51:22 +0300

    Фиксы подфиксы

--- app/src/main/java/com/kachaninc/adventureofcrazymage/ui/game/GameViewModel.kt
+++ app/src/main/java/com/kachaninc/adventureofcrazymage/ui/game/GameViewModel.kt
@@ -3,6 +3,6 @@ package com.kachaninc.adventureofcrazymage.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
 
--- app/src/main/java/com/kachaninc/adventureofcrazymage/ui/main/MainFragment.kt
+++ app/src/main/java/com/kachaninc/adventureofcrazymage/ui/main/MainFragment.kt
@@ -1,32 +1,35 @@
 package com.kachaninc.adventureofcrazymage.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kachaninc.adventureofcrazymage.R
 import com.kachaninc.adventureofcrazymage.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    private val binding by lazy { FragmentMainBinding.inflate(layoutInflater) }
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-            binding.button.setOnClickListener {
+        binding.buttonRules.setOnClickListener {
             Toast.makeText(requireContext(), "on", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_mainFragment_to_gameFragment)
         }
-
-      //   requireActivity().findViewById<TextView>(R.id.buttonRules)
-
     }
-
-
 }
